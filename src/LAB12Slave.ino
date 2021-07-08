80a49718f5bf24176172f060a020a3b12214c474
VishalKaushik123 <84299956+VishalKaushik123@users.noreply.github.com>
2021-07-08 15:43:32 -0500

    Update LAB12Slave.ino

--- src/LAB12Slave.ino
+++ src/LAB12Slave.ino
@@ -1,56 +1,60 @@
 /*
- * Project LAB12Slave
- * Description:
- * Author:
- * Date:
+ * Project Lab12Slave
+ * Description: SLAVE
+ * Author: Vikash Kaushik
+ * Date: 7/8/21
  */
+//SLAVE
 SYSTEM_MODE(MANUAL);
 SYSTEM_THREAD(ENABLED);
-
+// transmitting and receiving data
+char rx[1];
+char tx[1];
 bool CS = false;
 void takeAction(uint8_t state)
-{
-  if (state)
   {
-    CS = true; // watch for this variable in loop and set it back to false once you detect it.
+    if (state)
+    {
+      CS = true; // watch for this variable in loop and set it back to false once you detect it.
+      
+    }
   }
-}
-
+  
 // setup() runs once, when the device is first turned on.
 void setup()
 {
-  pinMode(D7, OUTPUT);
-  pinMode(D5, INPUT);
+  pinMode(D7, OUTPUT); // LED
+  pinMode(D5, INPUT); // Button
+  
   SPI1.setBitOrder(MSBFIRST);
   SPI1.setDataMode(SPI_MODE0);
   SPI1.onSelect(takeAction);
 
   SPI1.begin(SPI_MODE_SLAVE, D6);
+
+  
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop()
 {
-  char rx[1];
-  char tx[1];
+  
+  
 
-  if (CS == true)
-  {
+  if(CS == true){
     CS = false;
     SPI1.transfer(tx, rx, 1, NULL);
-
-    if (rx[0] == '0')
-    {
-      digitalWrite(D7, LOW);
+    if(rx[0] == '0'){
+      digitalWrite(D7,LOW);
     }
-    else if (rx[0] == '1')
-    {
-      digitalWrite(D7, HIGH);
+    if(rx[0] == '1'){
+      digitalWrite(D7,HIGH);
     }
-    else if (rx[0] == '?')
-    {
-      tx[0] = digitalRead(D5);
+    if(rx[0] == '?'){
+      
+      tx[0] = digitalRead(D5); 
       SPI1.transfer(tx, rx, 1, NULL);
+
     }
   }
 }
